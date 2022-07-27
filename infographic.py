e06a7bb0a696eeb690283a9fa287e00fa38065ea
Russell <russengle6@gmail.com>
2022-07-27 16:29:22 -0400

    infographic start

--- infographic.py
+++ infographic.py
@@ -1,21 +1,31 @@
 from data_object import Data
 import matplotlib.pyplot as plt
-
+import numpy as np
 
 
 
 class Infographic():
     def __init__(self, pnumber: int):
         self.table = Data(pnumber).pivot()
-        self.x_dic = {}
+        self.dic = {}
         for i in self.table.index.unique(level='VarName'):
-            self.x_dic[i] = self.table.loc[(i,)]
+            self.dic[i] = self.table.loc[(i,)]
         self.fig = plt.figure(figsize=(10, 12), dpi=300, constrained_layout=True)
-        self.gs = self.fig.add_gridspec(nrow=2, ncols=2)
+        self.gs = self.fig.add_gridspec(nrows=2, ncols=2)
 
     def add_plot(self, index, key):
-        ax = self.fig.add_subplot(self.gs[index+1,0:2])
-        ax.plot()
+        ax = self.fig.add_subplot(self.gs[index,0:2])
+        x = np.array((self.dic[key].index))
+        y = self.table['VarValue'][key].to_list()
+        ax.plot(x,y)
+
+    def show(self):
+        plt.show()
+
+info = Infographic(607)
+info.add_plot(1,'PT270_Value')
+info.show()
+
 
 
 
--- test.py
+++ test.py
@@ -1,47 +1,45 @@
-# from data_object import Data
-# import matplotlib.pyplot as plt
-# import numpy as np
-# import matplotlib.dates as dates
-# import datetime
-#
-# file = Data(607)
-# table = file.pivot()
-# #'2022-07-24 19:55:32'
-# #'2022-07-26 15:25:40'
-#
-# dic = {}
-# for i in table.index.unique(level='VarName'):
-#     dic[i] = table.loc[(i,)]
-#
-# startdate = datetime.datetime.strptime('2022-07-25 8:55:32','%Y-%m-%d %H:%M:%S')
-# enddate = datetime.datetime.strptime('2022-07-26 15:25:40','%Y-%m-%d %H:%M:%S')
-#
-# fig,ax = plt.subplots()
-# fully = table['VarValue']['PT270_Value'].to_list()
-# fullx = (dic['PT270_Value'].index).to_pydatetime()
-# x_data = np.array((dic['PT270_Value'].index))
-#
-# y1 = []
-# x1 = []
-# for i in range(0,len(x_data)):
-#     if  startdate<= fullx[i] <= enddate:
-#         x1.append(fullx[i])
-#         y1.append(fully[i])
-# x1 = np.arange(0,len(x))
-# A = np.vstack([x1,np.ones(len(x))])
-#
-# m, b = np.linalg.lstsq(A.T, y, rcond = None)[0]
-# plt.plot(fullx,fully,label='Original Data')
-# plt.plot(x,m*x1+b,'r',label="Fitted Line")
-# ax.plot_date(fullx, fully, '-')
-#
-# ax.xaxis.set_minor_locator(dates.HourLocator(interval=2))   # every 4 hours
-# ax.xaxis.set_minor_formatter(dates.DateFormatter('%H:%M'))  # hours and minutes
-# ax.xaxis.set_major_locator(dates.DayLocator(interval=1))    # every day
-# ax.xaxis.set_major_formatter(dates.DateFormatter('\n%m-%d-%Y'))
-#
-# plt.legend()
-# plt.show()
+from data_object import Data
+import matplotlib.pyplot as plt
+import numpy as np
+import matplotlib.dates as dates
+import datetime
 
-for i in range(10,-1,-1):
-    print(i)
+file = Data(607)
+table = file.pivot()
+#'2022-07-24 19:55:32'
+#'2022-07-26 15:25:40'
+
+dic = {}
+for i in table.index.unique(level='VarName'):
+    dic[i] = table.loc[(i,)]
+
+startdate = datetime.datetime.strptime('2022-07-25 8:55:32','%Y-%m-%d %H:%M:%S')
+enddate = datetime.datetime.strptime('2022-07-26 15:25:40','%Y-%m-%d %H:%M:%S')
+
+fig,ax = plt.subplots()
+fully = table['VarValue']['PT270_Value'].to_list()
+fullx = (dic['PT270_Value'].index).to_pydatetime()
+x_data = np.array((dic['PT270_Value'].index))
+
+y1 = []
+x1 = []
+for i in range(0,len(x_data)):
+    if  startdate<= fullx[i] <= enddate:
+        x1.append(fullx[i])
+        y1.append(fully[i])
+x1 = np.arange(0,len(x))
+A = np.vstack([x1,np.ones(len(x))])
+
+m, b = np.linalg.lstsq(A.T, y, rcond = None)[0]
+plt.plot(fullx,fully,label='Original Data')
+plt.plot(x,m*x1+b,'r',label="Fitted Line")
+ax.plot_date(fullx, fully, '-')
+
+ax.xaxis.set_minor_locator(dates.HourLocator(interval=2))   # every 4 hours
+ax.xaxis.set_minor_formatter(dates.DateFormatter('%H:%M'))  # hours and minutes
+ax.xaxis.set_major_locator(dates.DayLocator(interval=1))    # every day
+ax.xaxis.set_major_formatter(dates.DateFormatter('\n%m-%d-%Y'))
+
+plt.legend()
+plt.show()
+
