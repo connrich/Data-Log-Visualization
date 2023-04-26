66a7a1408f9235b397020ed1a74b09e3d86b2e96
Connor <connor.richards@quantum-technology.com>
2023-04-26 15:55:01 -0700

    Added view menu and convenience functions

---
+++ gui_resources/graph_selection_widget.py
@@ -0,0 +1,108 @@
+from PyQt5.QtWidgets import QPushButton, QWidget, QGridLayout, QButtonGroup
+from PyQt5.QtCore import QEvent
+
+
+class SelectorButton(QPushButton):
+    unselected_ss = '''
+        QPushButton {
+            background-color: gray;
+            color: white;
+            border: 2px solid transparent;
+            border-radius: 8px;
+            padding: 8px 16px;
+        }
+    '''
+
+    selected_ss = '''
+        QPushButton {
+            background-color: #cbe8f5;
+            color: white;
+            border: 2px solid black;
+            border-radius: 8px;
+            padding: 8px 16px;
+        }
+    '''
+
+    def __init__(self, row: int, col: int, parent: QWidget=None) -> None:
+        super().__init__()
+        # Save location and parent information
+        self.row = row
+        self.col = col
+        self.parent = parent
+        
+        # Show button as unselected initially
+        self.setStyleSheet(self.unselected_ss)
+
+    def selected(self) -> None:
+        '''
+        Shows the button as selected
+        '''
+        self.setStyleSheet(self.selected_ss)
+
+    def unselected(self) -> None:
+        '''
+        Shows the button as unselected
+        '''
+        self.setStyleSheet(self.unselected_ss)
+
+    def enterEvent(self, a0: QEvent) -> None:
+        '''
+        Triggers when the mouse enters the bounds of the button
+        '''
+        self.parent.buttonHovered(self)
+        return super().enterEvent(a0)
+
+    def leaveEvent(self, a0: QEvent) -> None:
+        '''
+        Triggers when the mouse leaves the bounds of the button
+        '''
+        self.parent.buttonUnhovered(self)
+        return super().leaveEvent(a0)
+
+
+
+class ButtonGrid(QWidget):
+
+    def __init__(self, rows: int, cols: int, parent: QWidget) -> None:
+        super().__init__()
+
+        self.parent = parent
+        print(self.parent)
+
+        # Main layout for the grid
+        self.layout = QGridLayout()
+        self.setLayout(self.layout)
+
+        # Iteratively create the buttons and save to the array
+        self.ButtonGroup = QButtonGroup()
+        self.ButtonGroup.buttonClicked.connect(lambda btn: self.parent.showGraphs(btn.row, btn.col))
+        for row in range(rows):
+            for col in range(cols):
+                btn = SelectorButton(row, col, self)
+                self.ButtonGroup.addButton(btn, row*3+col)
+                self.layout.addWidget(btn, row, col)
+    
+    def buttonAt(self, row: int, col: int) -> SelectorButton:
+        '''
+        Returns the widget at (row, col)
+        '''
+        return self.layout.itemAt(row*self.layout.columnCount() + col).widget()
+    
+    def buttonHovered(self, btn: SelectorButton):
+        '''
+        Called by a child button when it is hovered over
+        '''
+        for row in reversed(range(btn.row+1)):
+            for col in reversed(range(btn.col+1)):
+                self.buttonAt(row, col).selected()
+    
+    def buttonUnhovered(self, btn: SelectorButton):
+        '''
+        Called by a child button when it is hovered over
+        '''
+        for row in reversed(range(btn.row+1)):
+            for col in reversed(range(btn.col+1)):
+                self.buttonAt(row, col).unselected()
+    
+    def buttonClicked(self, btn: SelectorButton) -> None:
+        self.parent.showGraphs(btn.row, btn.col)
--- modbus_gui.py
+++ modbus_gui.py
@@ -15,7 +15,7 @@ PyQt packages
 from PyQt5.QtWidgets import QApplication, QMainWindow, QDockWidget, QWidget, \
                             QAction, QFileDialog, QGridLayout, QToolBar, \
                             QCheckBox, QPushButton, QLabel, QMenuBar, QMenu, \
-                            QWidgetAction, QLineEdit, QVBoxLayout
+                            QWidgetAction, QLineEdit, QVBoxLayout, QComboBox
 from PyQt5.QtCore import Qt, QPointF
 from PyQt5.QtGui import QIcon, QCloseEvent
 
@@ -28,12 +28,9 @@ import pandas as pd
 Custom packages
 '''
 from gui_resources.graph_widget import GraphWidget
-from gui_resources.data_selection_widget import DataSelectionWidget
-from gui_resources.style import StyleSheet as SS
-from gui_resources.error_message import ErrorMessage
-from gui_resources.date_time_input_widget import DateTimeInput
 from gui_resources.resource_path import resource_path
 from gui_resources.menu_text_input import MenuTextInputWidget
+from gui_resources.graph_selection_widget import ButtonGrid
 from modbus_resources.ModbusLogging import ModbusLogger
 
 
@@ -52,15 +49,17 @@ class MODBUS_GUI(QMainWindow):
         self.setWindowTitle('QuantumView')
         self.setWindowIcon(QIcon(resource_path('gui_resources\images\Quantum_icon.png')))
 
+        # Construct and add the multigraph widget to the window
+        #TODO implement across all classes
+        self.GraphArraySize = (3, 3)
+        self.Graphs = MultiGraphWidget()
+        self.setCentralWidget(self.Graphs)
+
         # Construct and add the menu to the window
         self.constructMenuBar()
 
         # Construct and add the tool bar to the window
         self.constructToolbar()
-
-        # Construct and add the graphs to the window
-        self.Graphs = MultiGraphWidget()
-        self.setCentralWidget(self.Graphs)
     
     def constructMenuBar(self) -> None:
         # Create/add menu bar
@@ -71,6 +70,11 @@ class MODBUS_GUI(QMainWindow):
         self.ConnectMenu = QMenu('Connect')
         self.constructConnectMenu()
         self.MenuBar.addMenu(self.ConnectMenu)
+
+        # Create/construct view menu
+        self.ViewMenu = QMenu('View')
+        self.constructViewMenu()
+        self.MenuBar.addMenu(self.ViewMenu)
 
     def constructConnectMenu(self) -> None:
         # Input for server IP
@@ -91,11 +95,25 @@ class MODBUS_GUI(QMainWindow):
         self.connectAction.setDefaultWidget(self.connectButton)
         self.ConnectMenu.addAction(self.connectAction)
 
-    def constructToolbar(self) -> None:
-        # Create/add the toolbar
-        self.ToolBar = QToolBar()
-        self.addToolBar(self.ToolBar)
+    def constructViewMenu(self) -> None:
+        # Construct the button selector for graph display
+        self.GraphView = ButtonGrid(*self.GraphArraySize, self.Graphs)
+        self.GraphViewContainer = QWidgetAction(self.GraphView)
+        self.GraphViewContainer.setDefaultWidget(self.GraphView)
+        self.ViewMenu.addAction(self.GraphViewContainer)
 
+    def constructToolbar(self) -> None:
+        pass
+        # # Create/add the toolbar
+        # self.ToolBar = QToolBar()
+        # self.addToolBar(self.ToolBar)
+
+        # # Add options for number of displayed graphs
+        # self.numGraphs = QComboBox()
+        # self.ToolBar.addWidget(self.numGraphs)
+        # self.numGraphs.addItem('1')
+        # self.numGraphs.addItem('9')
+        # self.numGraphs.currentTextChanged.connect(lambda num: self.Graphs.showGraphs(int(num)))
 
 
 class MultiGraphWidget(QWidget):
@@ -107,20 +125,67 @@ class MultiGraphWidget(QWidget):
         self.setLayout(self.layout)
 
         # Iteratively create graphs
-        self.createGraphs()
+        self.initGraphs()
 
-    def createGraphs(self) -> None:
+    def initGraphs(self) -> None:
+        '''
+        Initializes the graphs in the layout
+        '''
         # Create 12 graphs and add them to the layout
         for row in range(3):
-            for col in range(4):
+            self.layout.setRowStretch(row, 1)
+            self.layout.setColumnStretch(row, 1)
+            for col in range(3):
                 graph = GraphWidget()
                 graph.hide()
                 self.layout.addWidget(graph, row, col)
-        # Only show 1 graph to start
-        self.graphAt(0, 0).show() 
+        self.showAllGraphs()
 
     def graphAt(self, row: int, col: int) -> GraphWidget:
+        '''
+        Returns the graph widget at the coordinate
+        (0, 0) is top left
+        '''
         return self.layout.itemAtPosition(row, col).widget() 
+
+    def showGraphs(self, row, col) -> None:
+        '''
+        Show a certain array of graphs 
+        '''
+        # self.hideAllGraphs()
+        print(row, col)
+    
+    def hideAllGraphs(self) -> None:
+        '''
+        Hides all graphs and reset row/column stretch
+        '''
+        # Reset column stretch
+        for i in range(self.layout.columnCount()):
+            self.layout.setColumnStretch(i, 0)
+        
+        # Reset row stretch
+        for i in range(self.layout.rowCount()):
+            self.layout.setRowStretch(i, 0)
+        
+        # Hide all widgets
+        for i in range(self.layout.count()):
+            self.layout.itemAt(i).widget().hide()
+    
+    def showAllGraphs(self) -> None:
+        '''
+        Shows all graphs
+        '''
+        # Set column stretch
+        for i in range(self.layout.columnCount()):
+            self.layout.setColumnStretch(i, 1)
+        
+        # Set row stretch
+        for i in range(self.layout.rowCount()):
+            self.layout.setRowStretch(i, 1)
+        
+        # Show all widgets
+        for i in range(self.layout.count()):
+            self.layout.itemAt(i).widget().show()
 
 
 
